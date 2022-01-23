dc1d2b0842f8f8b50399a94a6449ef99d14ea851
pedroaway <ppgabriell00@gmail.com>
2022-01-23 10:28:08 -0300

    NavBar e Páginas prontas

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 
-def home(requeest):
-    return HttpResponse('helo world')
+def home(request):
+    return render(request, "index.html")
+
+def inquilinos(request):
+    return render(request, "inquilinos.html")
+
+def unidades(request):
+    return render(request, "unidades.html")
 
